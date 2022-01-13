1001ca4246d312aa709b51e0fddb01293344814f
Developsteven <steven.perez099@gmail.com>
2022-01-13 14:24:22 -0500

    Formulario Login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { LoginPagesComponent } from './pages/login-pages/login-pages.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/modules/auth/pages/login-pages/login-pages.component.ts
+++ src/app/modules/auth/pages/login-pages/login-pages.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { getMaxListeners } from 'process';
 
 @Component({
   selector: 'app-login-pages',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPagesComponent implements OnInit {
 
+  formLogin: FormGroup = new FormGroup({})
   constructor() { }
 
   ngOnInit(): void {
+    this.formLogin = new FormGroup(
+      {
+        email: new FormControl('', [
+          Validators.required,
+          Validators.email
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4)
+        ])
+      }
+    )
   }
 
+  sendLogin(): void{
+    const body = this.formLogin.value
+    console.log('=>', body);
+  }
 }
